182f932dcf93c8921626900d39c60058638a1d47
akirahashimoto <hashimoto7965@gmail.com>
2021-04-25 15:56:24 +0900

    almost finished

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, abort, request, Response
+from flask import Flask, abort, request, Response, send_file
+from flask_cors import CORS
 
 from queue import Queue
 import functools
@@ -13,13 +14,10 @@ import slackweb
 import pandas as pd
 import os.path as osp
 
-
 slack = slackweb.Slack(f"https://hooks.slack.com/services/{slack_token}")
 
-
 app = Flask(__name__)
-
-
+CORS(app)
 
 singleQueue = Queue(maxsize=1)
 
@@ -57,16 +55,15 @@ def hello():
 @multiple_control(singleQueue)
 @app.route('/estimate', methods=["POST"])
 def app_estimate():
-    test_csv = request.files.get('file')
-    if test_csv is None:
-        return abort(404)
     if request.headers.getlist("X-Forwarded-For"):
         ip = request.headers.getlist("X-Forwarded-For")[0]
     else:
         ip = request.remote_addr
     slack.notify(text=f"[ESTIMATE] : \n from {ip}")
-
-    if isinstance(test_csv, FileStorage) and test_csv.content_type == 'text/csv':
+    test_csv = request.files.get('test_csv')
+    if test_csv is None:
+        return abort(404)
+    if test_csv.content_type == 'text/csv' or test_csv.content_type == 'application/vnd.ms-excel':
         test_df = pd.read_csv(test_csv)
     else:
         return abort(404, {'code': 'Wrong file', 'message': 'the file is not csv please send me csv'})
@@ -74,20 +71,23 @@ def app_estimate():
     test_encoded = preprocess(test_df)
     test_preds_df = estimate(test_encoded)
     submit_df = postprocess(test_preds_df, test_df)
-    test_df.to_csv(osp.join(DATA_DIR, f'dip_input_{time.time()}.csv'), index=False, header=True)
-    submit_df.to_csv(osp.join(OUTPUT_DIR, f'dip_estimete_{time.time()}.csv'), index=False, header=True)
+    temp_time = time.time()
+    test_df.to_csv(osp.join(DATA_DIR, f'dip_input_{temp_time}.csv'), index=False, header=True)
+    submit_df.to_csv(osp.join(OUTPUT_DIR, f'dip_estimete_{temp_time}.csv'), index=False, header=True)
 
-    textStream = StringIO()
-    submit_df.to_csv(textStream, index=False, header=True)
     output_name = f"estimate_{test_csv.filename}"
     if output_name[-4:] != ".csv":
         output_name += ".csv"
+
+    textStream = StringIO()
+    submit_df.to_csv(textStream, index=False, header=True)
 
     return Response(
         textStream.getvalue(),
         mimetype="text/csv",
         headers={"Content-disposition":
                 f"attachment; filename={output_name}"})
+
 
 
 @multiple_control(singleQueue)
@@ -120,4 +120,4 @@ def app_estimate_test():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
